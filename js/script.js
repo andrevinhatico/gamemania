273e56a7eb1a7c0545199624638b19fa1d22633a
Andre Ribeiro <andrebahia7@gmail.com>
2022-01-17 23:39:30 -0300

    Implementando JS e a pagina de consoles-produtos

--- js/script.js
+++ js/script.js
@@ -29,3 +29,110 @@ function cadastrarEmail() {
 
     console.log(emailDigitado, senhaDigitada);
 }
+
+
+function renderizarProdutos() {
+
+    let espaco = document.getElementById("espaco-titulo")
+
+    let listaProdutos = [
+
+        {
+            imagem: "img/xbox.svg",
+            titulo: "Playstation",
+            descricao:"PlayStation 4 e PS5",
+            preco: "R$ 3.000,00"
+        },
+
+        {
+            imagem: "img/xbox.svg",
+            titulo: "Nitendo",
+            descricao:"Super Nitendo",
+            preco: "R$ 2.100,00"
+        },
+
+        {
+            imagem: "img/xbox.svg",
+            titulo: "Switch",
+            descricao:"Game super",
+            preco: "R$ 1.000,00"
+        },
+
+        {
+            imagem: "img/xbox.svg",
+            titulo: "Atari",
+            descricao:"Pura Nostalgia",
+            preco: "R$ 500,00"
+        },
+
+        {
+            imagem: "img/xbox.svg",
+            titulo: "Tec-Toy",
+            descricao:"Master e Mega",
+            preco: "R$ 1.200,00"
+        },
+
+        {
+            imagem: "img/xbox.svg",
+            titulo: "Gradiente",
+            descricao:"Turbo Game",
+            preco: "R$ 1.200,00"
+        }
+    ]
+    
+    
+    let template = "";
+    
+    for (let index = 0; index < listaProdutos.length; index++) {
+        const produto = listaProdutos[index];
+        
+        console.log(produto)
+        
+        template +=  `<div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch" >
+
+                <div class="card text-center bg-light">
+                    <img src="${produto.imagem}" alt="imagem console" class="card-img-top">
+
+                    <div class="card-header preco-projeto">
+                        ${produto.preco}
+                    </div>
+
+                    <div class="card-body">
+                        <h5 class="card-title">${produto.titulo}</h5>
+                        <p class="card-text">${produto.descricao} </p>
+                    </div>
+
+                    <div class="card-footer">
+
+                        <form class="d-block">
+                            <button class="btn btn-projeto">
+                                Adicionar ao Carrinho
+                            </button>
+                        </form>
+                        <small class="text-success">Em estoque</small>
+
+                    </div>
+
+                </div>
+
+            </div>`
+                        
+                    }         
+                
+                
+             
+                 espaco.innerHTML = template
+    
+    }    
+        
+        
+        
+        
+        
+        
+        
+        
+               
+        
+        
+       
